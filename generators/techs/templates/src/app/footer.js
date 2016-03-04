0dad109f743e69c1b3a9ba2c4b01da6fed02acef
Micaël <micael.mbagira@icloud.com>
2016-03-04 11:11:21 +0100

    Fix linting

--- generators/techs/templates/src/app/footer.js
+++ generators/techs/templates/src/app/footer.js
@@ -11,5 +11,5 @@ module.exports = ng.Component({
     '</footer>'
 })
 .Class({
-  constructor: function() {}
+  constructor: function () {}
 });
--- generators/techs/templates/src/app/header.js
+++ generators/techs/templates/src/app/header.js
@@ -15,5 +15,5 @@ module.exports = ng.Component({
     '</header>'
 })
 .Class({
-  constructor: function() {}
+  constructor: function () {}
 });
