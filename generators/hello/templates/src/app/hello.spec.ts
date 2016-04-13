8490a613839f362b0d65e5d1f2671228277dcfa2
Micaël <micael.mbagira@icloud.com>
2016-04-13 17:41:38 +0200

    Add Hello World unit test

--- generators/hello/templates/src/app/hello.spec.ts
+++ generators/hello/templates/src/app/hello.spec.ts
@@ -1,8 +1,22 @@
-import {Hello} from './hello';
+/// <reference path="../../typings/main.d.ts"/>
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts"/>
 
-describe('hello component', function () {
-  it('should render hello world', function () {
-    const hello = new Hello();
-    expect(hello.hello).to.equal('Hello World!');
-  });
+<% if (modules === 'webpack') { -%>
+import 'zone.js/dist/zone';
+<% } -%>
+import {Hello} from './hello';
+import {describe, it, expect, injectAsync, TestComponentBuilder, setBaseTestProviders} from 'angular2/testing';
+import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+
+describe('hello component', () => {
+  setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
+
+  it('should render hello world', injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+    return tcb.createAsync(Hello)
+      .then(fixture => {
+        fixture.detectChanges();
+        const hello = fixture.nativeElement;
+        expect(hello.querySelector('h1').textContent).toBe('Hello World!');
+      });
+  }));
 });
