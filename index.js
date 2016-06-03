ba08321b833363f8709939075094a15983502a99
Micaël <micael.mbagira@icloud.com>
2016-06-03 10:38:15 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  app: require.resolve('./generators/app'),
+  component: require.resolve('./generators/component'),
+  directive: require.resolve('./generators/directive'),
+  pipe: require.resolve('./generators/pipe'),
+  service: require.resolve('./generators/service'),
+  hello: require.resolve('./generators/hello'),
+  techs: require.resolve('./generators/techs'),
+  todoMVC: require.resolve('./generators/todoMVC')
+};
