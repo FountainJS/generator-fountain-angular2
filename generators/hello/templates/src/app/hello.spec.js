44f19c35e91721c6aacb87f6c91fe5b7cda24d75
Micaël <micael.mbagira@icloud.com>
2016-07-07 10:03:42 +0200

    Move out test configuration

--- generators/hello/templates/src/app/hello.spec.js
+++ generators/hello/templates/src/app/hello.spec.js
@@ -4,9 +4,6 @@ require('zone.js/dist/zone');
 require('zone.js/dist/async-test');
 var Hello = require('./hello');
 var ngTest = require('@angular/core/testing');
-var ngPlatformDynamic = require('@angular/platform-browser-dynamic/testing');
-
-ngTest.setBaseTestProviders(ngPlatformDynamic.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, ngPlatformDynamic.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
 
 describe('hello component', function () {
   it('should render hello world', ngTest.async(ngTest.inject([ngTest.TestComponentBuilder], function (tcb) {
