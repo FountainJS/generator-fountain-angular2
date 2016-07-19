5d23dac669e45726711e67427299ce7e05b16937
Micaël <micael.mbagira@icloud.com>
2016-07-19 16:09:45 +0200

    Add test

--- test/directive/index.js
+++ test/directive/index.js
@@ -18,14 +18,14 @@ test(`Call this.copyTemplate 2 times without 'dir' option`, t => {
   const spy = chai.spy.on(context, 'copyTemplate');
   TestUtils.call(context, 'writing');
   expect(spy).to.have.been.called.exactly(2);
-  t.true(context.copyTemplate['src/app/directive.js'].length > 0);
-  t.true(context.copyTemplate['src/app/directive.spec.js'].length > 0);
+  t.true(context.copyTemplate['src/app/myDirective.js'].length > 0);
+  t.true(context.copyTemplate['src/app/myDirective.spec.js'].length > 0);
 });
 
 test(`Call this.copyTemplate 2 times with 'dir' option`, t => {
   const spy = chai.spy.on(context, 'copyTemplate');
   TestUtils.call(context, 'writing', {dir: 'game'});
   expect(spy).to.have.been.called.exactly(2);
-  t.true(context.copyTemplate['src/app/game/directive.js'].length > 0);
-  t.true(context.copyTemplate['src/app/game/directive.spec.js'].length > 0);
+  t.true(context.copyTemplate['src/app/game/myDirective.js'].length > 0);
+  t.true(context.copyTemplate['src/app/game/myDirective.spec.js'].length > 0);
 });
